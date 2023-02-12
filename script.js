6a10cccd1a28d50893139cbd46baa40ef81a7972
muhammadrakan204 <rakanmuhammad21@gmail.com>
2023-02-12 17:24:18 +0700

    finish

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector(".navbar-nav");
 const lima = document.querySelectorAll(".lima");
 const seratus = document.querySelector(".seratus");
+const link = document.querySelectorAll(".link");
+const btn = document.querySelector(".btn-see-more");
+const testi = document.querySelectorAll(".btn-active");
+
+for (let a = 0; a < testi.length; a++) {
+  btn.addEventListener("click", function () {
+    testi[a].classList.remove("see-more");
+    btn.classList.add("hapus-btn");
+  });
+}
 
 hamburgerMenu.addEventListener("click", function () {
   navbar.classList.toggle("active");
@@ -17,6 +27,12 @@ document.addEventListener("click", function (e) {
     navbar.classList.remove("active");
   }
 });
+
+for (let a = 0; a < link.length; a++) {
+  link[a].addEventListener("click", function () {
+    navbar.classList.remove("active");
+  });
+}
 
 function myFunction(x) {
   if (x.matches) {
